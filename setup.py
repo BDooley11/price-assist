083b996651ce2a652085b2c834aa2d4cc6f93a5c
BDooley11 <55091575+BDooley11@users.noreply.github.com>
2020-02-08 18:16:18 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+requirements = [requests,bs4,xlxwriter,xlrb,time]
+
+setup(name="priceAssist",
+      version="0.1",
+      description="used car price check from done deal",
+      url="",
+      author="Barry Dooley",
+      author_email="barry.dooley@ucd.ie",
+      licence="GPL3",
+      packages=['priceAssist'],
+      install_requires=requirements,
+      entry_points={
+        'console_scripts':['run priceAssist=priceAssist.main:main']
+        }
+      )
